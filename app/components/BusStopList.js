28a571dbb7f48fc47fcb4b93325bf08cb0107d9a
AizenSousuke <puteranikdimas@yahoo.com.sg>
2020-12-30 23:30:08 +0800

    Added menu item

--- app/components/BusStopList.js
+++ app/components/BusStopList.js
@@ -1,11 +1,10 @@
-import React from "react";
-import { Button } from "react-native";
+import React, { useState } from "react";
 import { Pressable } from "react-native";
 import { View, Text } from "react-native";
-import { Icon, ListItem } from "react-native-elements";
-import { TouchableHighlight } from "react-native-gesture-handler";
+import { Icon, ListItem, Overlay, Tooltip } from "react-native-elements";
 
 const BusStopList = ({ name, address, code }) => {
+	const [OverlayVisible, setOverlayVisible] = useState(false);
 	return (
 		<ListItem bottomDivider>
 			<ListItem.Content>
@@ -15,11 +14,41 @@ const BusStopList = ({ name, address, code }) => {
 				</ListItem.Subtitle>
 			</ListItem.Content>
 			<Pressable
-				onPress={() => console.log("Touched option")}
+				onPress={() => setOverlayVisible(true)}
 				android_ripple={{ borderless: true }}
 			>
 				<View>
 					<Icon name="more-vert" />
+					<Overlay
+						isVisible={OverlayVisible}
+						onBackdropPress={() =>
+							setOverlayVisible(!OverlayVisible)
+						}
+					>
+						<View>
+							<ListItem>
+								<ListItem.Title>Add to:</ListItem.Title>
+							</ListItem>
+							<ListItem
+								onPress={() => {
+									setOverlayVisible(false);
+									console.log("Adding to Going Out List");
+								}}
+							>
+								<ListItem.Subtitle>Going Out</ListItem.Subtitle>
+							</ListItem>
+							<ListItem
+								onPress={() => {
+									setOverlayVisible(false);
+									console.log("Adding to Going Home List");
+								}}
+							>
+								<ListItem.Subtitle>
+									Going Home
+								</ListItem.Subtitle>
+							</ListItem>
+						</View>
+					</Overlay>
 				</View>
 			</Pressable>
 		</ListItem>
